afb683eba7c00c10dbc7ccc98d51a0b5e4c7b754
Tony Spiro <tspiro@tonyspiro.com>
2016-08-14 22:08:50 -0500

    footer edit

--- components/Partials/Footer.js
+++ components/Partials/Footer.js
@@ -5,13 +5,8 @@ export default class Footer extends Component {
     return (
       <div>
         <div>
-          To create your own polls:
+          To create your own polls sign up free at <a href="https://cosmicjs.com" target="_blank">Cosmic JS</a>
         </div>
-        <div>1. Sign up free at <a href="https://cosmicjs.com" target="_blank">Cosmic JS</a></div>
-        <div>2. After sign up, install this Voting App</div>
-        <div>3. Add / edit your own polls</div>
-        <div>4. Deploy your voting app to the web</div>
-        <div style={ S('h-100') }></div>
       </div>
     )
   }
