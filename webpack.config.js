5e64162d74977492cc71e0ed9192d9ff8dc71d7a
Tony Spiro <tspiro@tonyspiro.com>
2016-09-09 22:21:35 -0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,7 @@ module.exports = {
   },
   plugins: [
     new webpack.DefinePlugin({
+      'process.env.COSMIC_BUCKET': JSON.stringify(process.env.COSMIC_BUCKET),
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
       'process.env.COSMIC_READ_KEY': JSON.stringify(process.env.COSMIC_READ_KEY),
       'process.env.COSMIC_WRITE_KEY': JSON.stringify(process.env.COSMIC_WRITE_KEY)
